f2832cf1aacae557b8745cd9e68b6fbbf673f78f
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-09-04 15:11:48 -0400

    Added index.js content for compoenents

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <header>
+      <h1 id="footer"> Footer Content Here!</h1>
+    </header>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <main>
+      <nav>About Me</nav>
+    </main>
+  );
+}
+
+export default Navigation;
